bade727066e69ff282f02047cf51e482cb7737f3
Maria Cosmen <mcosme00@gmail.com>
2022-06-15 10:11:01 +0900

    // Changed layout and added icons

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const nextWeek = document.getElementById("next-week");
 const todayInfo = document.getElementById("today-info");
 const searchButton = document.getElementById("search");
 let input = document.getElementById("input");
+let icon = document.getElementById("icon");
+
+//
 
 // WEATHER VARIABLES
 
@@ -29,38 +32,92 @@ searchButton.addEventListener("click", (e) => {
   axios.get(forecast).then(showWeather);
   //axios.get(forecast).then(showForecast);
 
-  // CLEARING INPUT
+  // clearing input value:
   input.value = "";
 });
 
 const showWeather = (response) => {
-  console.log(response);
+  let data = response.data;
+  console.log(response.data);
   //GETTING DATA FROM API:
-  const newCity = response.data.location.name;
-  const newCondition = response.data.current.condition.text;
-  const newTemp = Math.floor(response.data.current.temp_c);
+  const newCity = data.location.name;
+  const newCondition = data.current.condition.text;
+  const newTemp = Math.floor(data.current.temp_c);
+  const newHtemp = Math.floor(data.forecast.forecastday[0].day.maxtemp_c);
+  const newLtemp = Math.floor(data.forecast.forecastday[0].day.mintemp_c);
 
   //UPDATE INFO IN HTML
   city.innerHTML = newCity;
   condition.innerHTML = newCondition;
   temp.innerHTML = `${newTemp}º`;
+  htemp.innerHTML = `H ${newHtemp}º`;
+  ltemp.innerHTML = `L ${newLtemp}º`;
 
-  //CREATING RANDOM HIGH & LOW TEMP VALUES (this weather api doesn't provide them)
-  htemp.innerHTML = `H ${newTemp + Math.floor(Math.random() * 3)}º`;
-  ltemp.innerHTML = `L ${newTemp - Math.floor(Math.random() * 3)}º`;
+  changeIcon(newCondition);
 };
 
 const showForecast = (forecast) => {
   console.log(forecast);
 };
 
-// HANDLE BOX SWITCH //
-nextDays.addEventListener("click", () => {
-  nextWeek.classList.remove("hidden");
-  todayInfo.classList.add("hidden");
-});
+// WEATHER CONDITION DESCRIPTIONS
 
-today.addEventListener("click", () => {
-  todayInfo.classList.remove("hidden");
-  nextWeek.classList.add("hidden");
-});
+const descriptions = {
+  sunny: ["Sunny"],
+  clear: ["Clear"],
+  cloudy: ["Partly cloudy", "Cloudy", "Overcast"],
+  rainy: [
+    "Patchy rain possible",
+    "Patchy light rain",
+    "Light rain",
+    "Light rain shower",
+    "Moderate rain at times",
+    "Moderate rain",
+    "Light freezing rain",
+  ],
+  heavyRain: [
+    "Heavy rain at times",
+    "Heavy rain",
+    "Moderate or heavy rain shower",
+    "Torrential rain shower",
+    "Moderate or heavy freezing rain",
+  ],
+  mist: ["Mist", "Fog", "Freezing fog"],
+  thunder: [
+    "Patchy light rain with thunder",
+    "Moderate or heavy rain with thunder",
+    "Thundery outbreaks possible",
+  ],
+  foggy: [""],
+  snowy: [
+    "Patchy snow possible",
+    "Patchy light snow",
+    "Light snow",
+    "Light snow showers",
+    "Patchy moderate snow",
+    "Moderate snow",
+  ],
+  heavySnow: [
+    "Patchy heavy snow",
+    "Heavy snow",
+    "Moderate or heavy snow showers",
+  ],
+  snowThunder: [
+    "Patchy light snow with thunder",
+    "Moderate or heavy snow with thunder",
+  ],
+};
+
+//ITERATE THROUGH ARRAY OBJECT:
+
+const changeIcon = (newCondition) => {
+  for (const key in descriptions) {
+    for (let i = 0; i < Object.keys(descriptions).length; i++) {
+      if (newCondition === descriptions[key][i]) {
+        icon.src = `./media/icons/${key}.svg`;
+        console.log(key);
+        console.log(icon);
+      }
+    }
+  }
+};
